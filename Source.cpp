89583583c9d4327f2879cda840e47fccdde7b1a7
Aayush <aad598@nyu.edu>
2018-11-23 22:24:27 +0400

    Final

--- Source.cpp
+++ Source.cpp
@@ -25,8 +25,8 @@ int main()
 	double WindChillTemperature;
 	
 	//asks for filename
-	cout << "Enter the filename: ";
-	cin >> fileName;
+	// cout << "Enter the filename: ";
+	// cin >> fileName;
 	
 	//calls the function to count the number of datapoint in first two lines
 	//will be used to dynamically allocate arrays
@@ -57,16 +57,17 @@ int main()
 		// 	cout << temps[i] << "  ";
 		// }
 		// cout << endl;
+
 		//gets temperature and windspeed value from user
 		userInput(apprTemp, apprWSpeed, tempInput, wSpeedInput, numTemp, numWSpeed, temps, windSpeed);
 		
 		//gets the correct index
 		indexTracing(apprTemp, apprWSpeed, indexTemps, indexWindSpeed, temps,windSpeed, numTemp, numWSpeed);
+		
 		// cout << indexTemps <<endl;
 		// cout << indexWindSpeed <<endl;
 		// cout <<apprTemp << "   " << indexTemps <<endl;
 		// cout << apprWSpeed << "   " << indexWindSpeed <<endl;
-
 
 		temperatureDrop(tempInput,indexTemps,indexWindSpeed,tempDrop, WindChillTemperature);
 
@@ -74,6 +75,7 @@ int main()
 
 		// cout << apprTemp <<endl;	
 		// cout <<apprWSpeed <<endl;
+
 		cout << "Do you wish to continue?\n";
 		cout << "Enter anything to continue.\n";
 		cout << "Enter \"exit\" to exit the program.\n";
@@ -132,6 +134,76 @@ void numDataPoints(string fileName, int &numTemp, int &numWSpeed)
 		inFile.get(charac);
 		if (charac == ' ' || charac == '\n')
 			numWSpeed++; //incremented everytime a space is encountered and when end of line is reached. This value denotes the number of data values in the line
+	}
+
+	charac='a';
+	int counter=0;
+	int i=1;
+	while (!inFile.eof())
+	{
+		inFile.get(charac);
+		if (charac==' ' || charac=='\n')
+			counter++;
+		if (charac=='\n')
+		{
+			if (counter==numTemp)
+			{
+				counter=0;
+				charac='a';
+			}
+			else if (counter>numTemp)
+			{
+				cout << "Number Of Temperature Drop Values  In Line " << i+2 << " Is Greater Than Number Of Temperature Values In The First Line\n";
+				counter=0;
+				exit(-1);
+			}
+			else if (counter<numTemp)
+			{
+				cout << "Number Of Temperature Drop Values In Line " << i+2 << " Is Less Than Number Of Temperature Values In The First Line\n";
+				counter=0;
+				exit(-1);
+			}
+			i++;
+
+		}
+
+
+	}
+
+	inFile.close();
+
+	inFile.open(fileName, ios::in);
+	if (inFile.fail())
+	{
+		cerr << "Error Opening File";
+		//Sleep(2000);
+		exit(-1);
+	}
+	charac='a';
+	int LineNum=0;
+	int numTempDropLines;
+	while (LineNum!=2)
+	{
+		inFile.get(charac);
+		if(charac=='\n')
+		{
+			LineNum++;
+			charac='a';
+		}
+	}
+	while (!inFile.eof())
+	{
+		inFile.get(charac);
+		if (charac=='\n')
+		{
+			numTempDropLines++;
+			charac='a';
+		}
+	}
+	if (numTempDropLines<numWSpeed || numTempDropLines>numWSpeed)
+	{
+		cout << "Number of lines of temperature drop values does not match the number values in the second line";
+		exit(-1);
 	}
 
 	inFile.close();
@@ -236,6 +308,8 @@ void userInput(int& apprTemp, int& apprWSpeed, double& tempInput, double& wSpeed
 		else if (wSpeedInput==windSpeed[i-1])
 			apprWSpeed=windSpeed[i-1];
 	}
+
+
 }
 
 void indexTracing(int apprTemp, int apprWSpeed, int& indexTemps,int& indexWindSpeed,int* temps, int* windSpeed, int numTemp, int numWSpeed)
